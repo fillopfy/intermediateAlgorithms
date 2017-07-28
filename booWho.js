fccac959c8ddd12f6c4acb9b93c623b44524d99c
Abhishek Tiwari <abhi456tm@gmail.com>
2017-07-28 10:18:27 +0530

    boolean primitive

---
+++ booWho.js
@@ -0,0 +1,11 @@
+//to check if the given str is boolean primitive(bool primitive: only true and false)
+
+function booWho(bool){
+
+
+return bool===true || bool===false;
+
+
+}
+
+//Or, return typeof bool==='boolean';
